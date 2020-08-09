084e30c8eff3437c3ba5a8534cc39e198c93743a
hannut91 <hannut91@gmail.com>
2020-08-09 20:26:33 +0900

    Add test for title
    Add test for title to check that title is renderd.

--- src/RetrospectivesPage.test.jsx
+++ src/RetrospectivesPage.test.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import RetrospectivesPage from './RetrospectivesPage';
+
+const { queryByText } = screen;
 
 describe('RetrospectivesPage', () => {
   it('renders retrospectives page', () => {
     render((
       <RetrospectivesPage />
     ));
+
+    expect(queryByText('무엇을 하셨나요?')).toBeInTheDocument();
   });
 });
